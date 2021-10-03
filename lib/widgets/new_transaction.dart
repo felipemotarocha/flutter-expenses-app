2ac33ff897425451420225c1adab50e97b7be244
Felipe Rocha <felipemota.rocha@gmail.com>
2021-10-03 19:35:07 -0300

    feat: add new transaction; styling improvements

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -9,6 +9,17 @@ class NewTransaction extends StatelessWidget {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
+  void _handleAddTransactionPress() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    handleAddTransactionPress(enteredTitle, enteredAmount);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -23,18 +34,18 @@ class NewTransaction extends StatelessWidget {
               decoration: const InputDecoration(
                 labelText: 'Title',
               ),
+              onSubmitted: (_) => _handleAddTransactionPress(),
             ),
             TextField(
               controller: amountController,
               decoration: const InputDecoration(
                 labelText: 'Amount',
               ),
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => _handleAddTransactionPress(),
             ),
             FlatButton(
-              onPressed: () => handleAddTransactionPress(
-                titleController.text,
-                double.parse(amountController.text),
-              ),
+              onPressed: _handleAddTransactionPress,
               child: const Text('Add Transaction'),
               textColor: Colors.purple,
             )

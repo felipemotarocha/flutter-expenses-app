8c70d6040d8118d7cf861198d6d3a0a15da4e152
Felipe Rocha <felipemota.rocha@gmail.com>
2021-10-09 16:16:55 -0300

    feat: start to create chart widget; calculate transactions amount per week day

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,44 @@
+import 'package:expenses_app/entities/transaction.dart';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  const Chart({Key? key, required this.recentTransactions}) : super(key: key);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+
+      var weekDayTotalAmount = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        final currentTransaction = recentTransactions[i];
+
+        final transactionBelongsToCurrentWeekDay =
+            currentTransaction.date.day == weekDay.day &&
+                currentTransaction.date.month == weekDay.month &&
+                currentTransaction.date.year == weekDay.year;
+
+        if (transactionBelongsToCurrentWeekDay) {
+          weekDayTotalAmount += currentTransaction.amount;
+        }
+      }
+
+      return {'day': DateFormat.E(weekDay), 'amount': weekDayTotalAmount};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
